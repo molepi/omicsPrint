c60aefbb064d3276e5e5791261dbc4420a50d38d
mviterson <mviterson@gmail.com>
2017-11-16 20:40:53 +0100

    adjust scale figure

--- examples/omicsPrint_siblings.R
+++ examples/omicsPrint_siblings.R
@@ -27,11 +27,9 @@ data <- alleleSharing(gt, relations = r)
 mismatches <- inferRelations(data)
 mismatches
 
+## Inspect Hardy-Weinberg filtering
 
-
-##
-
-alpha <- seq(0, 100, by=1)
+alpha <- seq(0, 1, by = .01)
 nsnps <- numeric(length(alpha))
 for(i in 1:length(alpha)) {
     x <- omicsPrint:::.pruning(gt, callRate=0.95, coverageRate=2/3, alpha = alpha[i], verbose=TRUE)
@@ -42,11 +40,13 @@ nrow(gt)
 
 
 pdf("InspectHWfiltering.pdf")
-
-plot(alpha/1000, 1000-nsnps, type = "b", log = "xy", panel.first=grid(col=1, equilogs=FALSE),
+par(mar=c(5,4,6,2))
+plot(alpha/1000, 1002-nsnps, type = "b",
+     log = "x", panel.first=grid(col=1, equilogs=FALSE),
      xlab = "Significant threshold (Bonferonni corrected)",
      ylab = "Number of SNPs",
      main = "Number of SNPs violating Hardy-Weinberg principle\n(at different significance levels)")
-
+at <- axTicks(1)
+axis(3, at, labels=at*1000)
 dev.off()
 
