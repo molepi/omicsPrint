816a331b94a395e2dc0e2c52b63dabfb04b8bc9c
mviterson <mviterson@gmail.com>
2017-11-25 07:02:02 +0100

    hw filtering figure

--- examples/omicsPrint_Fam_Males.R
+++ examples/omicsPrint_Fam_Males.R
@@ -1,7 +1,32 @@
 ##try to filter probes like in vignette
 
-library(GEOquery)
-gse <- getGEO("GSE73412", GSEMatrix = T)
+getGEO <- function(..., tmax = 5, verbose = TRUE){
+    require(GEOquery)
+    gse <- NULL
+    tryMax <- tmax
+    tries <- 0
+    if(verbose)
+        message("Downloading data from GEO ...")
+    while(!inherits(gse,'list') | tries >= tryMax) {
+        tries <- tries + 1
+        gse <- tryCatch(
+            GEOquery::getGEO(...),
+            error = function(e) {
+                Sys.sleep(5)
+                if(verbose) {
+                    message(e)
+                    message(' ... retrying')
+                }
+            })
+    }
+    return(gse)
+}
+
+##library(GEOquery)
+##gse <- getGEO("GSE73412", GSEMatrix = TRUE)
+
+
+
 library(omicsPrint)
 library(SummarizedExperiment)
 se <- makeSummarizedExperimentFromExpressionSet(gse$GSE73412_series_matrix.txt.gz)
@@ -29,7 +54,7 @@ fun <- function(x) {
     ##print(x)
     if (x["Yfam"] == x["Xfam"]){
         roles <- x[c("Xrole", "Yrole")]
-     ##print(roles)
+        ##print(roles)
         if ("(son)" %in% roles && "(father)" %in% roles){
             return("father/son")
         } else if ("(son)" %in% roles && "(grandfather)" %in% roles) {
@@ -62,3 +87,27 @@ gt <- beta2genotype(se, assayName = "exprs")
 data <- alleleSharing(gt, relations = r, rel.col = "relationship", verbose = TRUE)
 mismatches <- inferRelations(data)
 mismatches
+
+
+## Inspect Hardy-Weinberg filtering
+
+alpha <- seq(0, 1, by = .01)
+nsnps <- numeric(length(alpha))
+for(i in 1:length(alpha)) {
+    x <- omicsPrint:::.pruning(gt, callRate=0.95, coverageRate=2/3, alpha = alpha[i], verbose=TRUE)
+    nsnps[i] <- nrow(x)
+}
+
+n <- nrow(gt)
+
+
+par(mar=c(5,4,6,2))
+plot(alpha/n, n-nsnps, type = "b",
+     log = "x", panel.first = grid(col=1, equilogs=FALSE),
+     xlab = "Significant threshold (Bonferonni corrected)",
+     ylab = "Number of SNPs",
+     main = "Number of SNPs violating Hardy-Weinberg principle\n(at different significance levels)")
+at <- axTicks(1)
+axis(3, at, labels=at*n)
+
+
